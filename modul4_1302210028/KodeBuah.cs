0d3576c08bd043ff6330e9dcdea1c932d75c7a90
rihanrh <>
2023-03-14 16:17:32 +0700

    Menambahkan class KodeBuah

--- modul4_1302210028/KodeBuah.cs
+++ modul4_1302210028/KodeBuah.cs
@@ -8,5 +8,20 @@ namespace modul4_1302210028
 {
     internal class KodeBuah
     {
+        public enum Buah
+        {
+            Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry
+            , Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+        }
+
+        public string getKodeBuah(Buah buah)
+        {
+            string[] KodeBuah =
+            {
+                "A00", "B00", "C00", "D00", "E00", "F00"
+                , "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00"
+            };
+            return KodeBuah[(int)buah];
+        }
     }
 }
--- modul4_1302210028/Program.cs
+++ modul4_1302210028/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using modul4_1302210028;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        KodeBuah cari = new KodeBuah();
+        Console.WriteLine("Kode Buah untuk buah " + KodeBuah.Buah.Apel + " adalah " + cari.getKodeBuah(KodeBuah.Buah.Apel));
+    }
+}
