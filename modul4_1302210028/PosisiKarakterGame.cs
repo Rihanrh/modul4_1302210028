5b1f698c3e7a34a3077e89016750dea8f207558e
rihanrh <>
2023-03-14 17:17:50 +0700

    Menambahkan class PosisiKarakterGame

---
+++ modul4_1302210028/PosisiKarakterGame.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210028
+{
+    internal class PosisiKarakterGame
+    {
+        public enum KaraState { Jongkok, Berdiri, Tengkurap, Terbang};
+        public enum Trigger { TombolW, TombolS, TombolX };
+
+        class transition
+        {
+            public KaraState prevState;
+            public KaraState nextState;
+            public Trigger trigger;
+
+            public transition(KaraState prevState, KaraState nextState, Trigger trigger)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.trigger = trigger;
+            }
+        }
+
+        transition[] transitions =
+        {
+            new transition(KaraState.Jongkok, KaraState.Berdiri, Trigger.TombolW),
+            new transition(KaraState.Jongkok, KaraState.Tengkurap, Trigger.TombolS),
+            new transition(KaraState.Berdiri, KaraState.Terbang, Trigger.TombolW),
+            new transition(KaraState.Berdiri, KaraState.Jongkok, Trigger.TombolS),
+            new transition(KaraState.Terbang, KaraState.Berdiri, Trigger.TombolS),
+            new transition(KaraState.Terbang, KaraState.Jongkok, Trigger.TombolX),
+            new transition(KaraState.Tengkurap, KaraState.Jongkok, Trigger.TombolW),
+        };
+
+        public KaraState currentState;
+
+        public KaraState getNextState(KaraState prevState, Trigger trigger)
+        {
+            KaraState nextState = prevState;
+
+            for (int i = 0; i < transitions.Length; i++)
+            {
+                if (transitions[i].prevState == prevState && transitions[i].trigger == trigger)
+                {
+                    nextState = transitions[i].nextState;
+                }
+            }
+            return nextState;
+        }
+
+        public void activeTrigger(Trigger trigger)
+        {
+            KaraState nextState = getNextState(currentState, trigger);
+            currentState = nextState;
+        }
+    }
+}
--- modul4_1302210028/Program.cs
+++ modul4_1302210028/Program.cs
@@ -7,7 +7,49 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        // Kode Buah
         KodeBuah cari = new KodeBuah();
         Console.WriteLine("Kode Buah untuk buah " + KodeBuah.Buah.Apel + " adalah " + cari.getKodeBuah(KodeBuah.Buah.Apel));
+        Console.WriteLine();
+
+        // Posisi Karakter Game
+        // NIM % 3 == 1
+        PosisiKarakterGame pkg = new PosisiKarakterGame();
+        pkg.currentState = PosisiKarakterGame.KaraState.Berdiri;
+
+        Console.WriteLine("Sekarang karakter: " + pkg.currentState);
+
+        Console.WriteLine("Pilihan: W, S, X (CASE SENSITIVE!)");
+        Console.WriteLine("Masukkan Input:");
+        string input = Console.ReadLine();
+
+        while (input != null)
+        {
+            if (input == "W")
+            {
+                pkg.activeTrigger(PosisiKarakterGame.Trigger.TombolW);
+                if (pkg.currentState == PosisiKarakterGame.KaraState.Berdiri)
+                {
+                    Console.WriteLine("posisi standby");
+                }
+            }
+            else if (input == "S")
+            {
+                pkg.activeTrigger(PosisiKarakterGame.Trigger.TombolS);
+                if (pkg.currentState == PosisiKarakterGame.KaraState.Tengkurap)
+                {
+                    Console.WriteLine("posisi istirahat");
+                }
+                if (pkg.currentState == PosisiKarakterGame.KaraState.Berdiri)
+                {
+                    Console.WriteLine("posisi standby");
+                }
+            }
+            else if (input == "X")
+            {
+                pkg.activeTrigger(PosisiKarakterGame.Trigger.TombolX);
+            }
+            string inputlagi = Console.ReadLine();
+        }
     }
 }
